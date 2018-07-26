ec1d9f3f6fb8e1def4ac1c06b968e44e7c88d706
root <naveen.basra@gmail.com>
2018-07-26 06:37:58 -0400

    modified:   Module/network/main.tf

--- Module/network/main.tf
+++ Module/network/main.tf
@@ -1,31 +1,14 @@
+variable subnet_map_a {
+type=list,
+value=["10.1.0.1/16","10.3.0.1/16","10.5.0.1/16","10.7.0.1/16","10.7.0.1/16", "10.11.0.1/16"]
+}
+ 
+variable subnet_map_b {
+value=["10.2.0.1/16","10.4.0.1/16","10.6.0.1/16","10.8.0.1/16","11.7.0.1/16","10.12.0.1/16"]
+}
 
-locals
-{
- subnet_map_a={
-   small="10.1.0.1/16"
-   medium="10.3.0.1/16"
-   large="10.5.0.1/16"
-   db="10.7.0.1/16"
-   service="10.7.0.1/16"
-   lcm="10.11.0.1/16"
- }
- subnet_map_b={
-   small="10.2.0.1/16"
-   medium="10.4.0.1/16"
-   large="10.6.0.1/16"
-   db="10.8.0.1/16"
-   service="11.7.0.1/16"
-   lcm="10.12.0.1/16"
- }
- subnet_names={
-   small="app_small"
-   medium="app_medium"
-   large="app_large"
-   db="db"
-   service="service"
-   lcm="lcm"
- }
-
+variable subnet_names{
+value=["app_small", "app_medium","app_large","db","service", "lcm"]
 }
 
 
@@ -33,6 +16,7 @@ resource "aws_vpc" "lcm" {
   cidr_block = "$var.cidr_block"
 	tags{
 	Name = "${var.env_id}_LCM_VPC}"
+        }
 }
 
 
@@ -51,19 +35,19 @@ resource "aws_vpc_dhcp_options_association" "ssm_dns_resolver" {
 
 
 module "subnet_a"{
-  count= "${length(local.subnet_names)"
+  count= "${length(local.subnet_names)}"
   source="../subnet"
-  subnet_name="${var.env_id}_${element(local.subnet.names,count.index)}_a"
-  subnet="${element(local.subnet.map_a,count.index)}"
-  vpc_id="{aws_vpc.lcm.id}
+  subnet_name="${var.env_id}_${element(${var.subnet_names},count.index)}_a"
+  subnet="${element(${var.subnet_map_a},count.index)}"
+  vpc_id="${aws_vpc.lcm.id}"
 }
 
 
 module "subnet_b"{
-  count= "${length(local.subnet_names)"
+  count= "${length(local.subnet_names)}"
   source="../subnet"
-  subnet_name="${var.env_id}_${element(local.subnet.names,count.index)}_b"
-  subnet="${element(local.subnet.map_b,count.index)}"
-  vpc_id="{aws_vpc.lcm.id}
+  subnet_name="${var.env_id}_${element(${var.subnet_names},count.index)}_b"
+  subnet="${element(${var.subnet_map_b},count.index)}"
+  vpc_id="${aws_vpc.lcm.id}"
 }
 
